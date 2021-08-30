2fc2e7cd8725d78d0a24e0bedf3504118f83a0f5
Crowdbotics <team@crowdbotics.com>
2021-08-30 21:48:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_mountain_30207Identifier",
+  url: "https://frosty_mountain_30207Identifier.botics.co",
+  api: "https://frosty_mountain_30207Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
